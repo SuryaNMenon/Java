c372396ebb72e6e9f069a58d32558bce5258b1e0
Surya <suryanmenon@ieee.org>
2021-05-26 14:02:04 +0530

    First assignment

---
+++ program1.java
@@ -0,0 +1,12 @@
+public class program1 {
+    public static void main(String[] args){
+        System.out.println("Name\tMailId\t\tEmployee Code\tPhone");
+        System.out.println("Umesh\tumesh@cse\tp03161\t\t25764728");        
+        System.out.println("Salil\tsalil@cse\tp03160\t\t25764728");        
+        System.out.println("Sree\tsree@cse\tp03159\t\t25754728");        
+        System.out.println("Krish\tkrish@cse\tp03168\t\t22764728");        
+        System.out.println("Surya\tsurya@cse\tp03163\t\t15764728"); 
+
+        System.out.println("\"Can we print '\\' with System.out.println() statement?\"");
+    }
+}
